d1c963897902040ff47a6b84b4d3f5186ebf9fa6
Juan Ceballos <juanestebanceballos@gmail.com>
2025-04-02 15:27:38 -0500

    Se añade el readme y se soluciona el ultimo bug

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,19 +1,21 @@
 CREATE TABLE IF NOT EXISTS franquicia (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sucursal (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     franquicia_id BIGINT,
     FOREIGN KEY (franquicia_id) REFERENCES franquicia(id)
 );
 
 CREATE TABLE IF NOT EXISTS producto (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     cantidad_en_stock INT NOT NULL,
     sucursal_id BIGINT,
     FOREIGN KEY (sucursal_id) REFERENCES sucursal(id)
 );
+
+
